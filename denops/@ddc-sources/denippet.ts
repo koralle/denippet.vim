6613f2b2dcdc9e9e7d3bddd4dc4f39a589bd1654
koralle <koralle+github@koralle-mgmg.com>
2026-05-13 08:14:22 +0900

    feat: update `@shougo/ddc-vim` to v10.3.0 (#8)
    * refactor(deps): migrate ddc_vim to @shougo/ddc-vim on JSR
    
    * feat: update
    
    * Revert "feat: update"
    
    This reverts commit c98ab02e134bef02b906e51b5875422946f64184.

--- denops/@ddc-sources/denippet.ts
+++ denops/@ddc-sources/denippet.ts
@@ -1,17 +1,13 @@
 import {
   BaseSource,
-  DdcGatherItems,
-  Item,
-  Previewer,
-} from "https://deno.land/x/ddc_vim@v5.0.1/types.ts";
-import {
   GatherArguments,
   GetPreviewerArguments,
   OnCompleteDoneArguments,
-} from "https://deno.land/x/ddc_vim@v5.0.1/base/source.ts";
+} from "jsr:@shougo/ddc-vim@10.3.0/source";
+import { DdcGatherItems, Item, Previewer } from "jsr:@shougo/ddc-vim@10.3.0/types";
 import { Denops, op } from "../denippet/deps/denops.ts";
-import { splitLines } from "../denippet/util.ts";
 import { lsputil } from "../denippet/deps/lsp.ts";
+import { splitLines } from "../denippet/util.ts";
 
 type Params = Record<PropertyKey, never>;
 
@@ -30,27 +26,28 @@ type CompletedEvent =
   | "undefined";
 
 export class Source extends BaseSource<Params> {
-  async gather({
-    denops,
-  }: GatherArguments<Params>): Promise<DdcGatherItems> {
+  async gather({ denops }: GatherArguments<Params>): Promise<DdcGatherItems> {
     // We have to go through Vim script to guarantee that Denippet is loaded.
-    return await denops.call(
-      "denippet#get_complete_items",
-      false,
-    ) as Item<UserData>[];
+    return await denops.call("denippet#get_complete_items", false) as Item<UserData>[];
   }
 
-  override async onCompleteDone({ denops, userData }: OnCompleteDoneArguments<Params, UserData>): Promise<void> {
+  override async onCompleteDone(
+    { denops, userData }: OnCompleteDoneArguments<Params, UserData>,
+  ): Promise<void> {
     // Not expanded if confirmed with additional input.
     const completed_event = await denops.eval("g:pum#completed_event")
       .catch(() => "undefined") as CompletedEvent;
+
     if (completed_event === "complete_done") {
       return;
-    } else if (completed_event === "undefined") {
+    }
+
+    if (completed_event === "undefined") {
       // native-ui
       const itemWord = await denops.eval(`v:completed_item.word`) as string;
       const ctx = await lsputil.LineContext.create(denops);
       const beforeLine = ctx.text.slice(0, ctx.character);
+
       if (!beforeLine.endsWith(itemWord)) {
         return;
       }
@@ -60,31 +57,34 @@ export class Source extends BaseSource<Params> {
     await denops.call("ddc#skip_next_complete");
   }
 
-  override async getPreviewer({ denops, item }: GetPreviewerArguments<Params, UserData>): Promise<Previewer> {
+  override async getPreviewer(
+    { denops, item }: GetPreviewerArguments<Params, UserData>,
+  ): Promise<Previewer> {
     const userData = item.user_data;
+
     if (userData == null) {
       return { kind: "empty" };
     }
+
     const contents: string[] = await this.snippetToString(denops, userData.denippet.body)
       .then(splitLines)
       .catch(() => []);
+
     if (contents.length > 0) {
       const filetype = await op.filetype.get(denops);
       contents.unshift("```" + filetype);
       contents.push("```");
     }
+
     if (userData.denippet.description) {
       contents.unshift(userData.denippet.description);
     }
+
     return { kind: "markdown", contents };
   }
 
-  async snippetToString(denops: Denops, body: string): Promise<string> {
-    return await denops.dispatch(
-      "denippet",
-      "snippetToString",
-      body,
-    ) as string;
+  private async snippetToString(denops: Denops, body: string): Promise<string> {
+    return await denops.dispatch("denippet", "snippetToString", body) as string;
   }
 
   params(): Params {
